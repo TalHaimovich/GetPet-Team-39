3c3b4b9cdbd59801bb9e8a82fc8f6b4d45a60a25
kumkum12345 <asif.kdosha@gmail.com>
2021-11-28 12:23:20 +0200

    initial design and stracture

---
+++ GetPet/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request, send_from_directory
+from flask.templating import render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def index():
+    return render_template("index.html")
+
+@views.route('/css/<path:path>')
+def serve_css(path):
+    return send_from_directory('css', path)
---
+++ README.md
@@ -0,0 +1,7 @@
+# GetPet
+
+This is a basic software engineering course project at Sami Shimon College, Beer Sheva Israel This project is a site for adoption and foster care for animals
+
+The authors: Ortal Nosik Mor Markrabs Tal Haimovich And adif kdosha
+
+The work environment in this project is: Html, css, JS FLASK, Python, SQLlite
