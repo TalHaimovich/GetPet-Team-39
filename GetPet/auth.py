0935ea1c6d8a687ae08b5904ed4a93463f17f50c
TalHaimovich <93153279+TalHaimovich@users.noreply.github.com>
2021-11-29 11:44:17 +0200

    defined register for user and saveing in data base

--- GetPet/auth.py
+++ GetPet/auth.py
@@ -1,18 +1,67 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-#from .models import User
+from flask import Flask, Blueprint, app, render_template, request, flash, redirect, url_for, send_from_directory
+import sqlalchemy
+from .models import User, BusinessUser,AssociationUser
 from werkzeug.security import generate_password_hash, check_password_hash
-#from . import db
-from flask_login import login_user, login_required, logout_user, current_user
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user,login_manager
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/registeruser',methods=['GET','POST'])
 def registeruser():
+    email = request.form.get('email')
+    password1 = request.form.get('psw')
+    password2 = request.form.get('psw-repeat')
+    user1 = User.query.filter_by(email=email).first()
+    user2 = BusinessUser.query.filter_by(email=email).first()
+    user3 = AssociationUser.query.filter_by(email=email).first()
+    if user1 or user2 or user3:
+        return render_template("registeruser.html", exists=True)
+        # user exists
+    elif password1!=password2:
+        flag=False
+        return render_template("register.html")
+
+    if request.method == 'POST' and email and password1:
+        new_user = User(email=email, password=password1)
+        db.session.add(new_user)
+        db.session.commit()
+
     return render_template("registeruser.html")
+@auth.route('/reg_asos',methods=['GET','POST'])
+
 
 @auth.route('/reg_asos',methods=['GET','POST'])
 def reg_asos():
+
+    email = request.form.get('email')
+    password1 = request.form.get('psw')
+    password2 = request.form.get('psw-repeat')
+    name = request.form.get('name')
+    adress = request.form.get('adress')
+    user = BusinessUser.query.filter_by(email=email).first()
+    user1 = User.query.filter_by(email=email).first()
+    user2 = BusinessUser.query.filter_by(email=email).first()
+    user3 = AssociationUser.query.filter_by(email=email).first()
+    if user1 or user2 or user3:
+        # user exists
+        return render_template("reg_asos.html", exists=True)
+    if password1!=password2:
+        flag=False
+        return render_template("register.html")
+
+
+    if request.method == 'POST' and email and password1:
+        new_user = AssociationUser(email=email, password=password1, name=name, adress=adress)
+        db.session.add(new_user)
+        db.session.commit()
+
     return render_template("reg_asos.html")
+
 
 @auth.route('/registerbussines',methods=['GET','POST'])
 def registerbussines():
