69a4687bda83756a2f2623fb336da11eca010557
AsifKdosha <81109792+AsifKdosha@users.noreply.github.com>
2021-12-20 20:06:18 +0200

    updated data base with extra fetuers

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+with app.app_context():
+    from .models import User, Post
+    db.create_all()
+    if User.query.filter_by(email="admin@admin.com").count() == 0:
+        hashed_password = bcrypt.generate_password_hash('admin').decode('utf-8')
+        default_user = User(name='admin', email="admin@admin.com", password=hashed_password, is_admin=True)
+        db.session.add(default_user)
+        db.session.commit()
 
 from flaskblog import routes
